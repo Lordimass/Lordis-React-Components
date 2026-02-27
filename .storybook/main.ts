e98df23e82f44f44ef11b558b13b8b74a5eb0678
Lordimass <lordimass@lordimass.net>
2026-02-27 16:44:52 +0000

    Installed `Storybook`

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(ts|tsx)"],
+  addons: [
+    "@chromatic-com/storybook",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+  ],
+  framework: "@storybook/react-vite",
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      shouldExtractValuesFromUnion: true,
+    },
+  },
+};
+export default config;
---
+++ .storybook/preview.ts
@@ -0,0 +1,40 @@
+import type { Preview } from "@storybook/react-vite";
+import { spyOn } from "storybook/test";
+
+const preview: Preview = {
+  parameters: {
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: "todo",
+    },
+    backgrounds: {
+      options: {
+        dark: { name: "Dark", value: "#333" },
+        light: { name: "Light", value: "#F7F9F2" },
+        beige: { name: "Beige", value: "#d7cfb5" },
+        darkRed: { name: "Dark Red", value: "#501212" },
+      },
+      grid: {
+        opacity: 0.2,
+      },
+    },
+    layout: "centered",
+  },
+  initialGlobals: {
+    backgrounds: { value: "dark", grid: true },
+  },
+
+  // Components will have automatically generated Autodocs entries: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+
+  async beforeEach() {
+    // Spy on logs so we can see them in Storybook.
+    spyOn(console, "log").mockName("console.log");
+    spyOn(console, "warn").mockName("console.warn");
+    spyOn(console, "error").mockName("console.error");
+  },
+};
+
+export default preview;
