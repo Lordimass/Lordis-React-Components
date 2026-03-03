94d6f785fde73449bb520e4fa4ec5531732ec42e
Lordimass <lordimass@lordimass.net>
2026-03-03 08:01:47 +0000

    Remove duplicated Vite test project

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,4 @@ const dirname =
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
-  test: {
-    projects: [],
-  },
 });
