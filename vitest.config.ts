ca683c67ea289e62498a93a5920b26140d6dc20c
Lordimass <lordimass@lordimass.net>
2026-03-25 15:01:28 +0000

    Storybook testing fixes

--- vitest.config.ts
+++ vitest.config.ts
@@ -36,7 +36,6 @@ export default mergeConfig(
               headless: true,
               instances: [{ browser: "chromium" }],
             },
-            setupFiles: ["./.storybook/vitest.setup.ts"],
           },
         },
       ],
