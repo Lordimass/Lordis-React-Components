5a0469711f38fed4f9a62e593d9d25995d476106
Lordimass <lordimass@lordimass.net>
2026-03-29 11:10:11 +0100

    Added basic consent mode functions

--- src/lib/ga/index.ts
+++ src/lib/ga/index.ts
@@ -1,3 +1,23 @@
 export * from "./init";
 export * from "./events";
 export * from "./types";
+
+/**
+ * Update consent to agree to Google Analytics cookies.
+ */
+export function acceptCookies() {
+  window.gtag("consent", "update", {
+    analytics_storage: "granted",
+  });
+  localStorage.setItem("consentModeAnswer", "accept");
+}
+
+/**
+ * Update consent to deny use of Google Analytics cookies.
+ */
+export function declineCookies() {
+  window.gtag("consent", "update", {
+    analytics_storage: "denied",
+  });
+  localStorage.setItem("consentModeAnswer", "decline");
+}
