1bd31628a833c15cf5be6382a4a6478e9a57f152
Lordimass <lordimass@lordimass.net>
2026-04-07 11:35:28 +0100

    Updates

---
+++ assets/Product.stories-DZy_edST.js
@@ -0,0 +1,32 @@
+import{c as p,a as d,d as i,f as u,e as m,g as l}from"./fakes-CX9V6Dtz.js";import"./Ticker-BFB_NmmJ.js";import"./iframe-hv_kgyvg.js";import"./preload-helper-PPVm8Dsz.js";import"./PageSelector-r5SyDtQc.js";import"./Price-9N_7NOQG.js";import"./Content-Dz0jWMvM.js";const L={component:l,args:{prod:u,horizontal:!1,quantityLocked:!1,admin:!1,forceVertical:!1},argTypes:{prod:{control:{disable:!0},table:{defaultValue:{summary:u.name}}},horizontal:{control:"boolean"},quantityLocked:{control:"boolean"},admin:{control:"boolean"},forceVertical:{control:"boolean"}},globals:{layout:""}},r={},o={args:{prod:m}},a={args:{horizontal:!0}},e={args:{prod:d}},t={args:{prod:d,horizontal:!0}},s={args:{prod:u,quantityLocked:!0}},c={args:{prod:p,quantityLocked:!0}},n={args:{prod:i}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    prod: fakeProduct2
+  }
+}`,...o.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    horizontal: true
+  }
+}`,...a.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    prod: fakeProductGroup
+  }
+}`,...e.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    prod: fakeProductGroup,
+    horizontal: true
+  }
+}`,...t.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    prod: fakeProduct,
+    quantityLocked: true
+  }
+}`,...s.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    prod: fakeBasketProduct,
+    quantityLocked: true
+  }
+}`,...c.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    prod: fakeProductNoImages
+  }
+}`,...n.parameters?.docs?.source}}};const b=["SingleProduct","SingleProductOutOfStock","SingleProductHorizontal","Group","GroupHorizontal","QuantityLocked","BasketProduct","NoImages"];export{c as BasketProduct,e as Group,t as GroupHorizontal,n as NoImages,s as QuantityLocked,r as SingleProduct,a as SingleProductHorizontal,o as SingleProductOutOfStock,b as __namedExportsOrder,L as default};
