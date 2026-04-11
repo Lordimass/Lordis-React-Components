1ea88114ee6dac2dde9894b150f2cc4e5e0224d9
Lordimass <lordimass@lordimass.net>
2026-04-11 08:58:01 +0100

    Updates

---
+++ assets/formatter-EIJCOSYU-Dr-NVJyY.js
@@ -0,0 +1,1 @@
+import{d as i,r as m}from"./blocks-CqOnPrLp.js";import{f as o}from"./iframe-DD18YwLJ.js";import"./preload-helper-PPVm8Dsz.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
---
+++ assets/index-7OLQi9Mz.js
@@ -0,0 +1,28 @@
+import{j as n}from"./iframe-DD18YwLJ.js";import{u as i,b as c,c as t}from"./blocks-CqOnPrLp.js";import"./preload-helper-PPVm8Dsz.js";const a=`# acceptCookies() \`<Function>\`
+
+> **acceptCookies**(): \`void\`
+
+Defined in: [ga/index.ts:8](https://github.com/Lordimass/Lordis-React-Components/blob/bbd5a4a2438b1d8081ffef8292a30a430ec4bcb0/src/lib/ga/index.ts#L8)
+
+Update consent to agree to Google Analytics cookies.
+
+## Returns
+
+\`void\`
+`,d=`# declineCookies() \`<Function>\`
+
+> **declineCookies**(): \`void\`
+
+Defined in: [ga/index.ts:18](https://github.com/Lordimass/Lordis-React-Components/blob/bbd5a4a2438b1d8081ffef8292a30a430ec4bcb0/src/lib/ga/index.ts#L18)
+
+Update consent to deny use of Google Analytics cookies.
+
+## Returns
+
+\`void\`
+`;function s(o){const e={h1:"h1",...i(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(c,{title:"lib/Google Analytics/Cookie Consent"}),`
+`,n.jsx(e.h1,{id:"cookie-consent",children:"Cookie Consent"}),`
+`,n.jsx("br",{}),`
+`,n.jsx(t,{children:a}),`
+`,n.jsx("br",{}),`
+`,n.jsx(t,{children:d})]})}function m(o={}){const{wrapper:e}={...i(),...o.components};return e?n.jsx(e,{...o,children:n.jsx(s,{...o})}):s(o)}export{m as default};
