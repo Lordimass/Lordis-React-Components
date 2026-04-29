321ee8ec378930e549c7671a53a449a88b3789e8
Lordimass <lordimass@lordimass.net>
2026-04-29 07:57:34 +0100

    Updates

---
+++ assets/Products.stories-VaTvPQ-E.js
@@ -0,0 +1,5 @@
+import{g as a}from"./fakes-DSaYn-ZN.js";import{c as o}from"./BasketManager-Dn5b9dG8.js";import"./iframe-Cj4BdFUb.js";import"./preload-helper-PPVm8Dsz.js";import"./Ticker-QYwQHHMZ.js";import"./PageSelector-CxFzPKba.js";import"./Price-Cs2EIEtd.js";import"./Content-wy2ueHLa.js";const g={component:o,args:{prods:a,trackViewItemListProps:{itemListId:"home_page_$page",itemListName:"Home Page $page"}},argTypes:{prods:{control:{disable:!0}},pageSize:{control:"number"},trackViewItemListProps:{control:"object"}},globals:{layout:""}},e={},r={args:{pageSize:3}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    pageSize: 3
+  }
+}`,...r.parameters?.docs?.source}}};const l=["Default","Paginated"];export{e as Default,r as Paginated,l as __namedExportsOrder,g as default};
