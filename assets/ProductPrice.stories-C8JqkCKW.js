c215cc0a961cffe884a201ad764e8a3a26bdde53
Lordimass <lordimass@lordimass.net>
2026-05-08 09:19:09 +0100

    Updates

---
+++ assets/ProductPrice.stories-C8JqkCKW.js
@@ -0,0 +1,13 @@
+import{P as s,a as t}from"./BasketManager-B2a62XBC.js";import{w as c}from"./iframe-CsQADwQM.js";import{f as p,a}from"./fakes-DL6CnMWK.js";import"./Ticker-3o6JmnH-.js";import"./PageSelector-Bkz3iTyX.js";import"./Price-BzJoB4mG.js";import"./Content-BSFTVdSo.js";import"./preload-helper-PPVm8Dsz.js";const f={component:t,globals:{backgrounds:"light"},argTypes:{prod:{control:{disable:!0}}},decorators:[c]},r={args:{prod:p}},o={args:{prod:a}},e={args:{prod:new s(a.products.slice(0,2))}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    prod: fakeProduct
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    prod: fakeProductGroup
+  }
+}`,...o.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    prod: new ProductGroup(fakeProductGroup.products.slice(0, 2))
+  }
+}`,...e.parameters?.docs?.source}}};const k=["Default","PriceRange","AllEqualPrices"];export{e as AllEqualPrices,r as Default,o as PriceRange,k as __namedExportsOrder,f as default};
---
+++ assets/initGA4-C1c9Zs1P.js
@@ -0,0 +1,28 @@
+import{j as e}from"./iframe-CsQADwQM.js";import{u as s,M as o,a}from"./blocks-BvFkJ0SB.js";import"./preload-helper-PPVm8Dsz.js";const r=`# initGA4() \`<Function>\`
+
+> **initGA4**(\`measurement_id\`, \`dev?\`): \`void\`
+
+Defined in: [src/lib/ga/init.ts:21](https://github.com/Lordimass/Lordis-React-Components/blob/447919f783f9e081434be9413643dc8c70b74d59/src/lib/ga/init.ts#L21)
+
+Initialises Google Analytics 4 with default denied settings until cookies are accepted.
+
+GA4 must be initialised before consent for Advanced mode, which sends cookie-less pings to track analytics without
+association with the user. This should be combined with acceptCookies and declineCookies when consent
+is gained (or declined) by the user to use cookies for analytics. Until then, only cookie-less pings will be used,
+which prevent insights into user behaviours because events are not associated with each other, and instead just exist
+as stand-alone events.
+
+This method should be called early in the flow. Before the root component is even initialised (i.e. in \`main.tsx\`)
+
+## Parameters
+
+| Parameter | Type |
+| ------ | ------ |
+| \`measurement_id\` | \`string\` |
+| \`dev?\` | \`boolean\` |
+
+## Returns
+
+\`void\`
+`;function i(n){return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"lib/Google Analytics/initGA4"}),`
+`,e.jsx(a,{children:r})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i()}export{h as default};
